2b079070d1e8d30800b548e3b60ebe3c36280f38
Tsara Sudrajat <tsara.tf@gmail.com>
2022-01-23 17:44:22 +0700

    Serve index.html

--- myApp.js
+++ myApp.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get("/", function(req, res) {
-  res.send("Hello Express");
+  absolutePath = __dirname + "views/index.html"
+  res.sendFile(absolutePath);
 });
 
  module.exports = app;
