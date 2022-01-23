8a2a9333ba24f671b73f9499824d2994b0f287bc
Tsara Sudrajat <tsara.tf@gmail.com>
2022-01-23 17:52:57 +0700

    ADd static asset path

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use('/public', express.static(__dirname + "/public"));
+
 app.get("/", function(req, res) {
   absolutePath = __dirname + "/views/index.html"
   res.sendFile(absolutePath);
