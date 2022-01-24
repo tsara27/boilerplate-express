92079afa86d3aa765600aeed903df4bccb221900
Tsara Sudrajat <tsara.tf@gmail.com>
2022-01-24 14:07:17 +0700

    Add middleware to log method, path, and ip

--- myApp.js
+++ myApp.js
@@ -4,7 +4,12 @@ dotenv.config();
 var express = require('express');
 var app = express();
 
-app.use('/public', express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+
+app.use("/", function(req, res, next) {
+  console.log(req.method + " " + req.path + " " + " - " + req.ip);
+  next();
+})
 
 app.get("/", function(req, res) {
   absolutePath = __dirname + "/views/index.html"
